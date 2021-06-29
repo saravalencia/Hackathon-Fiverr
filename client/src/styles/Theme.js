67a3af749be202741368180ae2a16afa734a3330
Manuel Marque <manuelmarq05@gmail.com>
2021-06-29 13:31:31 +0200

    stylecomponent

--- client/src/styles/Theme.js
+++ client/src/styles/Theme.js
@@ -2,12 +2,25 @@ import { ThemeProvider } from "styled-components";
 
 const theme = {
 	colors: {
-		powderWhite: "#FFFDF9",
-		persianGreen: "#105e00",
-		lightBlue: "#3104b8",
-		onyx: "#36313D",
-		fiverrGreen: "#1CBF73"
+		green: "#1dbf73",
+		greenLight: "#d0f7e6",
+		greenMedium: "#00732e",
+		greenDark: "#003912",
+		yellow: "#d0e500",
+		yellowLight: "#f1f4cb",
+		yellowMedium: "#687200",
+		yellowDark: "#254200",
+		pink: "#ff80ae",
+		pinkLight: "#ffd3e2",
+		pinkMedium: "#be5272",
+		pinkDark: "#4d1727",
+		orange: "#ff7640",
+		orangeLight: "#ffe0d4",
+		orangeMedium: "#8f2900",
+		orangeDark: "#421300",
+		onyx: "#36313D"
 	},
+	
 	fontSizes: {
 		small: "1em",
 		medium: "2em",
