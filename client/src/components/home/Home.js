ca480eb41f7e4ecc05f526f854ba5c530ec1b281
Alopmel <78558722+Alopmel@users.noreply.github.com>
2021-06-29 13:29:52 +0200

    responsive and pc

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -6,13 +6,28 @@ import home from '../home/background.jpg'
 const DivHome = styled.div `
 background-image: url(${home});
 background-size: cover;
+background-repeat: no-repeat;
+background-position: center;
 width: 100%;
-height: 650px;
+height: 800px;
 
-`
+
+ @media screen and (max-width: 700px){
+        width: 100%;
+        height: 600px;
+        align-items: center;
+    }
+
+`;
+
 const ImgLogo = styled.img `
-width: 219px;
-height: 141px;
+    width: 501px;
+    height: 274px;
+
+ @media screen and (max-width: 700px){
+        width: 269px;
+        height: 163px;
+    }
 `;
 
 const DivContainer = styled.div `
@@ -22,24 +37,45 @@ justify-content: center;
 align-items: center;
 `
 const H1 = styled.h1`
-padding: 8px;
-font-family: 'Josefin Sans', sans-serif;
-font-size: 3.5rem;
+padding: 29px;
+font-family: 'Josefin Sans',sans-serif;
+font-size: 6rem;
 background-color: rgb(220 219 219 / 50%);
 text-align: center;
-width: 80%;
+
+@media screen and (max-width: 700px){
+  padding: 8px;
+  font-family: 'Josefin Sans', sans-serif;
+  font-size: 3.5rem;
+  background-color: rgb(220 219 219 / 50%);
+  text-align: center;
+  width: 80%;
+    }
 
 `
 const Select = styled.select`
-width: 150px;
-height: 30px;
+width: 264px;
+height: 52px;
+font-size: 27px;
+border: 2px solid;
+border-radius: 26px;
+padding-left: 66px;
+
+ @media screen and (max-width: 700px){
+        width: 150px;
+        height: 30px;
+        font-size: 1rem;
+        padding-left: 27px;
+    }
 `
 
 function Home() {
     return (
       <DivHome  >
+        <DivContainer>
+         <ImgLogo src={gifTransparent}  alt='gif'/>  
+        </DivContainer>
         
-        <ImgLogo src={gifTransparent}  alt='gif'/> 
         <DivContainer>
         <H1>
           Find your Match!
