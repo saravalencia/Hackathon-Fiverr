8e4eadf023355ff5326f8acda096c111220d18c2
Axhar85 <azhararshad_ntu@yahoo.com>
2021-06-29 16:22:35 +0200

    created navbar and hamburger menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,14 @@
-import { Switch, Route } from "react-router-dom";
-import Navbar from "./components/organisms/Navbar";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
-import Footer from "./components/organisms/Footer";
+  
+import React from 'react';
+import Navbar from './components/Nav/Navbar';
 
-const App = () => {
-
-    return (
-        <>
-            <header>
-                <h1>Starter template</h1>
-            </header>
-            <Navbar />
-            <Switch>
-                <Route
-                    exact path="/"
-                    render={props =>
-                      <Home {...props} />
-                    }
-                />
-                <Route
-                    path="/about"
-                    render={props =>
-                      <About {...props} />
-                    }
-                />
-                <Route
-                    path="/contact"
-                    render={props =>
-                      <Contact {...props} />
-                    }
-                />
-            </Switch>
-            <Footer />
-        </>
-    )
-};
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+     
+    </div>
+  );
+}
 
 export default App;
