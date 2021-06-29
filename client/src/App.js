255b6fde82382a0b25e298c591fec4281e85b44a
Sara Valencia <sara.valencia.m@gmail.com>
2021-06-29 15:08:42 +0200

    merged

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,8 @@
 import { Switch, Route } from "react-router-dom";
 import React, {Fragment} from "react";
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
-import Home from "./components/Home";
-import Questions from "./components/questions/Questions";
-=======
 import Home from "./components/home/Home";
-import Question1 from "./components/questions/Question1";
-import Question2 from "./components/questions/Question2";
-import Question3 from "./components/questions/Question3";
-import Question4 from "./components/questions/Question4";
->>>>>>> main
+import Questions from "./components/questions/Questions";
 
 
 
