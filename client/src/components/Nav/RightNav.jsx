4c69a19931f6ec55236e09195a849d2be3fcad9c
Axhar85 <azhararshad_ntu@yahoo.com>
2021-06-29 21:57:08 +0200

    fixed the error in hamburger menu

--- client/src/components/Nav/RightNav.jsx
+++ client/src/components/Nav/RightNav.jsx
@@ -10,14 +10,6 @@ const Ul = styled.ul`
   font-family: Domine;
   font-weight: bold;
 
-  
-
-  li {
-    padding: 18px 10px;
-    &:hover {
-     color: #29b34c;
-    }
-  
   @media (max-width: 768px) {
     flex-flow: column nowrap;
     background-color: #108b44;
@@ -29,13 +21,13 @@ const Ul = styled.ul`
     width: 300px;
     padding-top: 3.5rem;
     transition: transform 0.3s ease-in-out;
-    li {
-      color: #fff;
     }
 
-  
+  li {
+    padding: 18px 10px;
+    &:hover {
+     color: #29b34c;
     }
-
   }
 `;
 
