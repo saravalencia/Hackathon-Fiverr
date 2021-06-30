a27156d8f25a9ea18a785b79394c6613b425490b
Sara Valencia <sara.valencia.m@gmail.com>
2021-06-30 13:28:44 +0200

    backup for the hackathon

--- client/src/components/questions/QuestionsFreelance.js
+++ client/src/components/questions/QuestionsFreelance.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import gifnoloop from '../../video/gifnoloop.gif'
 import styled from 'styled-components'
 import gifTransparent from '../../video/gifTransparent.gif'
 
